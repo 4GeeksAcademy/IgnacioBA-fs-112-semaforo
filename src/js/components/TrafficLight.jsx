d75b2d735a57770f1f89cd72c8637075dc85a70b
IgnacioBA9 <150173378+IgnacioBA9@users.noreply.github.com>
2025-06-13 10:45:16 +0000

    [fix:]TrafficLight margins fixed

--- src/js/components/TrafficLight.jsx
+++ src/js/components/TrafficLight.jsx
@@ -14,20 +14,20 @@ const TrafficLight = () => {
             <div className="d-flex flex-column align-items-center bg-dark p-3 rounded" style={{ width: "100px" }}>
                 <div
                     onClick={() => changeColor("red")}
-                    className={`rounded-circle bg-danger ${isActive("red")}`}
+                    className={`rounded-circle bg-danger ${isActive("red")} my-2`}
                     style={{ width: "60px", height: "60px" }}
 
                 ></div>
 
                 <div
                     onClick={() => changeColor("yellow")}
-                    className={`rounded-circle bg-warning ${isActive("yellow")}`}
+                    className={`rounded-circle bg-warning ${isActive("yellow")}my-2`}
                     style={{ width: "60px", height: "60px" }}
                 ></div>
 
                 <div
                     onClick={() => changeColor("green")}
-                    className={`rounded-circle bg-success ${isActive("green")}`}
+                    className={`rounded-circle bg-success ${isActive("green")}my-2`}
                     style={{ width: "60px", height: "60px" }}
                 ></div>
             </div>
